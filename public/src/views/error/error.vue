89d73efb747416cf93cf08db4b0d7869f4b7ac2a
htcChain <15189707660@163.com>
2018-05-14 13:44:31 +0800

    add frontend project

---
+++ public/src/views/error/error.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: "error",
+  beforeCreate () {
+    document.querySelector('body').setAttribute('style', 'background-image: url(/static/img/404.jpg);background-size: cover;');
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
